2f373ec8676af308a015db755ef0677ab410da2c
markyv18 <markvanakkeren@gmail.com>
2016-12-02 08:54:55 -0700

    Update merge_sort.rb

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,1 +1,21 @@
 
+require 'pry'
+
+class MergeSort
+  def sort(letters)
+    length = (letters.length) / 2
+    length_front = length - 1 
+    length_back = length
+    first_half = letters[0..length_front]
+    second_half = letters[length_back..-1]
+    binding.pry
+  end
+end
+
+
+
+
+
+
+sorter = MergeSort.new
+sorter.sort(["d", "b", "a", "c"])
